be2ee175eec2aabfffeb1ff3bcbed85181afee44
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-24 22:48:45 -0500

    feat: implemented function for next ten numbers

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import os
 import csv
 
+def next_ten_numbers(num):
+    return ",".join(str(num + i) for i in range(1, 11))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def test_should_return_next_ten_numbers():
     assert next_ten_numbers(5) == "6,7,8,9,10,11,12,13,14,15"
     assert next_ten_numbers(0) == "1,2,3,4,5,6,7,8,9,10"
